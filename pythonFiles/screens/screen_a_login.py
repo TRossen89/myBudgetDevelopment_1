f3a9ab3ca3bf76abfc59701b9078054d031a6c9a
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2023-11-09 01:31:59 +0100

    work with rounded text input

--- pythonFiles/screens/screen_a_login.py
+++ pythonFiles/screens/screen_a_login.py
@@ -1,19 +1,42 @@
 from kivy.uix.screenmanager import Screen
 #from pythonFiles.persistence.db_connection import DBConnection
 from pythonFiles.persistence.user_mapper import UserMapper
+from pythonFiles.text_inputs.a_textinput_standard import TextInput_Standard
 
 
 
 class Login(Screen):
 
+    def __init__(self, **kwargs):
+        super(Login, self).__init__(**kwargs)
+
+        self.username = TextInput_Standard(.5, .7, False, "Username")
+        self.password = TextInput_Standard(.5, .5, True, "Password")
+
+        self.add_widget(self.username)
+        self.add_widget(self.password)
+
 
     def login(self):
 
-        username = self.ids.username.text
-        password = self.ids.password.text
+        #TextInput:
+         #   id: username
+          #  pos_hint: {"center_x": .5, "center_y":.7}
+           # size_hint: (.6, .07)
+
+        #TextInput:
+         #   id: password
+          #  pos_hint: {"center_x": .5, "center_y":.5}
+           # size_hint: (.6, .07)
 
 
-        loginStatus = UserMapper.login_request(username, password)
+
+
+        #username = self.ids.username.text
+        #password = self.ids.password.text
+
+
+        loginStatus = UserMapper.login_request(self.username.get_text(), self.password.get_text())
 
         if loginStatus == "connection_failed":
             print("Connection failed")
